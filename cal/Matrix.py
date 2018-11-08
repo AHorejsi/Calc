f8eb81ee478e2932ddd58ddb03bb6ee2e66766be
AHorejsi <alex.horejsi59@gmail.com>
2018-11-08 12:55:39 -0800

    Implementing Matrix type as one dimensional array

--- cal/Matrix.py
+++ cal/Matrix.py
@@ -1,15 +1,26 @@
 class Matrix:
-    def __init__(self, table):
-        self.table = table
+    def __init__(self, table, rowLength=None, columnLength=None):
+        if (rowLength is None) and (columnLength is None):
+            self.table = []
+            self.rowLength = len(table)
 
-    def rowLength(self):
-        return len(self.table)
+            if self.rowLength == 0:
+                self.columnLength = 0
+            else:
+                self.columnLength = len(self.table[0])
 
-    def columnLength(self):
-        if self.rowLength() == 0:
-            return 0
+            for row in table:
+                self.table += row
         else:
-            return len(self.table[0])
+            self.table = table
+            self.rowLength = rowLength
+            self.columnLength = columnLength
 
-    def __iter__(self):
-        return self.table.__iter__()
+    def equalDimensions(self, matrix):
+        return self.rowLength == matrix.rowLength and self.columnLength == matrix.columnLength
+
+    def multipliable(self, matrix):
+        return self.columnLength == matrix.rowLength
+
+    def __getitem__(self, coordinates):
+        return self.table[coordinates[0] * self.columnLength + coordinates[1]]
