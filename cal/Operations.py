5ef65699bd092d7660a41c56efeec85c1246a98a
AHorejsi <alex.horejsi59@gmail.com>
2018-11-08 12:09:35 -0800

    Implemented vector and quaternion types

--- cal/Operations.py
+++ cal/Operations.py
@@ -1,3 +1,247 @@
+from cal.Vector import Vector
+from cal.Quaternion import Quaternion
+
+
 def addition(leftOperand, rightOperand):
     typeOfLeft = type(leftOperand)
     typeOfRight = type(rightOperand)
+
+    if typeOfLeft is Vector:
+        # The below "if" statements indicate what types can be added to Vectors
+
+        if typeOfRight is Vector:
+            return vectorPlusVector(leftOperand, rightOperand)
+    elif typeOfLeft is Quaternion:
+        # The below "if" statements indicate what types can be added to Quaternions
+
+        if (typeOfRight is int) or (typeOfRight is float):
+            return quaternionPlusReal(leftOperand, rightOperand)
+        elif typeOfRight is complex:
+            return quaternionPlusComplex(leftOperand, rightOperand)
+        elif typeOfRight is Quaternion:
+            return quaternionPlusQuaternion(leftOperand, rightOperand)
+
+    raise TypeError(str(typeOfLeft) + " + " + str(typeOfRight) + " is not possible")
+
+
+def vectorPlusVector(leftVector, rightVector):
+    if not leftVector.equalDimensions(rightVector):
+        raise ArithmeticError("Vectors must be of equal dimensions to be added together")
+
+    point = []
+
+    for value1, value2 in zip(leftVector, rightVector):
+        point.append(value1 + value2)
+
+    return Vector(point)
+
+
+def quaternionPlusReal(leftQuaternion, rightReal):
+    return Quaternion(leftQuaternion.real + rightReal,
+                      leftQuaternion.imag,
+                      leftQuaternion.imag1,
+                      leftQuaternion.imag2)
+
+
+def quaternionPlusComplex(leftQuaternion, rightComplex):
+    return Quaternion(leftQuaternion.real + rightComplex.real,
+                      leftQuaternion.imag + rightComplex.imag,
+                      leftQuaternion.imag1,
+                      leftQuaternion.imag2)
+
+
+def quaternionPlusQuaternion(leftQuaternion, rightQuaternion):
+    return Quaternion(leftQuaternion.real + rightQuaternion.real,
+                      leftQuaternion.imag + rightQuaternion.imag,
+                      leftQuaternion.imag1 + rightQuaternion.imag1,
+                      leftQuaternion.imag2 + rightQuaternion.imag2)
+
+
+def subtraction(leftOperand, rightOperand):
+    typeOfLeft = type(leftOperand)
+    typeOfRight = type(rightOperand)
+
+    if typeOfLeft is Vector:
+        # The below "if" statements indicate what types can be subtracted from Vectors
+
+        if typeOfRight is Vector:
+            return vectorMinusVector(leftOperand, rightOperand)
+    elif typeOfLeft is Quaternion:
+        # The below "if" statements indicate what types can be
+
+        if (typeOfRight is int) or (typeOfRight is float):
+            return quaternionMinusReal(leftOperand, rightOperand)
+        elif typeOfRight is complex:
+            return quaternionMinusComplex(leftOperand, rightOperand)
+        elif typeOfRight is Quaternion:
+            return quaternionMinusQuaternion(leftOperand, rightOperand)
+
+    raise TypeError(str(typeOfLeft) + " - " + str(typeOfRight) + " is not possible")
+
+
+def vectorMinusVector(leftVector, rightVector):
+    if not leftVector.equalDimensions(rightVector):
+        raise ArithmeticError("Vectors must be of equal dimensions to be subtracted from each other")
+
+    point = []
+
+    for value1, value2 in zip(leftVector, rightVector):
+        point.append(value1 - value2)
+
+    return Vector(point)
+
+
+def quaternionMinusReal(leftQuaternion, rightReal):
+    return Quaternion(leftQuaternion.real - rightReal,
+                      leftQuaternion.imag,
+                      leftQuaternion.imag1,
+                      leftQuaternion.imag2)
+
+
+def quaternionMinusComplex(leftQuaternion, rightComplex):
+    return Quaternion(leftQuaternion.real - rightComplex.real,
+                      leftQuaternion.imag - rightComplex.imag,
+                      leftQuaternion.imag1,
+                      leftQuaternion.imag2)
+
+
+def quaternionMinusQuaternion(leftQuaternion, rightQuaternion):
+    return Quaternion(leftQuaternion.real - rightQuaternion.real,
+                      leftQuaternion.imag - rightQuaternion.imag,
+                      leftQuaternion.imag1 - rightQuaternion.imag1,
+                      leftQuaternion.imag2 - rightQuaternion.imag2)
+
+
+def multiplication(leftOperand, rightOperand):
+    typeOfLeft = type(leftOperand)
+    typeOfRight = type(rightOperand)
+
+    if typeOfLeft is Vector:
+        # The below "if" statements indicate what types can be multiplied to Vectors
+
+        if (typeOfRight is int) or (typeOfRight is float):
+            return vectorTimesReal(leftOperand, rightOperand)
+    elif typeOfLeft is Quaternion:
+        # The below "if" statements indicate what types can be multiplied to Quaternions
+
+        if (typeOfRight is int) or (typeOfRight is float):
+            return quaternionTimesReal(leftOperand, rightOperand)
+        elif typeOfRight is complex:
+            return quaternionTimesComplex(leftOperand, rightOperand)
+        elif typeOfRight is Quaternion:
+            return quaternionTimesQuaternion(leftOperand, rightOperand)
+
+    raise TypeError(str(typeOfLeft) + " * " + str(typeOfRight) + " is not possible")
+
+
+def vectorTimesReal(leftVector, rightReal):
+    point = []
+
+    for value in leftVector:
+        point.append(value * rightReal)
+
+    return Vector(point)
+
+
+def quaternionTimesReal(leftQuaternion, rightReal):
+    return Quaternion(leftQuaternion.real * rightReal,
+                      leftQuaternion.imag * rightReal,
+                      leftQuaternion.imag1 * rightReal,
+                      leftQuaternion.imag2 * rightReal)
+
+
+def quaternionTimesComplex(leftQuaternion, rightComplex):
+    return Quaternion(leftQuaternion.real * rightComplex.real - leftQuaternion.imag * rightComplex.imag,
+                      leftQuaternion.real * rightComplex.imag + leftQuaternion.imag * rightComplex.real,
+                      leftQuaternion.imag1 * rightComplex.real + leftQuaternion.imag2 * rightComplex.imag,
+                      -leftQuaternion.imag1 * rightComplex.imag + leftQuaternion.imag2 * rightComplex.real)
+
+
+def quaternionTimesQuaternion(leftQuaterion, rightQuaternion):
+    return Quaternion(leftQuaterion.real * rightQuaternion.real - leftQuaterion.imag * rightQuaternion.imag -
+                      leftQuaterion.imag1 * rightQuaternion.imag1 - leftQuaterion.imag2 * rightQuaternion.imag2,
+                      leftQuaterion.real * rightQuaternion.imag + leftQuaterion.imag * rightQuaternion.real -
+                      leftQuaterion.imag1 * rightQuaternion.imag2 + leftQuaterion.imag2 * rightQuaternion.imag1,
+                      leftQuaterion.real * rightQuaternion.imag1 + leftQuaterion.imag * rightQuaternion.imag2 +
+                      leftQuaterion.imag1 * rightQuaternion.real - leftQuaterion.imag2 * rightQuaternion.imag,
+                      leftQuaterion.real * rightQuaternion.imag2 - leftQuaterion.imag * rightQuaternion.imag1 +
+                      leftQuaterion.imag1 * rightQuaternion.imag + leftQuaterion.imag2 * rightQuaternion.real)
+
+
+def division(leftOperand, rightOperand):
+    typeOfLeft = type(leftOperand)
+    typeOfRight = type(rightOperand)
+
+    if typeOfLeft is Vector:
+        # The below "if" statements indicate what types can be divided from Vectors
+
+        if (typeOfRight is int) or (typeOfRight is float):
+            return vectorDividedByReal(leftOperand, rightOperand)
+    elif typeOfLeft is Quaternion:
+        # The below "if" statements indicate what types can be divided from Quaternions
+
+        if (typeOfRight is int) or (typeOfRight is float):
+            return quaternionDividedByReal(leftOperand, rightOperand)
+        elif typeOfRight is complex:
+            return quaternionDividedByComplex(leftOperand, rightOperand)
+        elif typeOfRight is Quaternion:
+            return quaternionDividedByQuaternion(leftOperand, rightOperand)
+
+    raise TypeError(str(typeOfLeft) + " / " + str(typeOfRight) + " is not possible")
+
+
+def vectorDividedByReal(leftVector, rightReal):
+    return vectorTimesReal(leftVector, 1 / rightReal)
+
+
+def quaternionDividedByReal(leftQuaternion, rightReal):
+    return Quaternion(leftQuaternion.real / rightReal,
+                      leftQuaternion.imag / rightReal,
+                      leftQuaternion.imag1 / rightReal,
+                      leftQuaternion.imag2 / rightReal)
+
+
+def quaternionDividedByComplex(leftQuaternion, rightComplex):
+    conjugateOfRightComplex = rightComplex.conjugate()
+    numerator = quaternionTimesComplex(leftQuaternion, conjugateOfRightComplex)
+    denominator = rightComplex * conjugateOfRightComplex
+    result = quaternionDividedByReal(numerator, denominator)
+
+    return result
+
+
+def quaternionDividedByQuaternion(leftQuaternion, rightQuaternion):
+    absoluteValueOfLeft = abs(leftQuaternion)
+
+    realOfResult = leftQuaternion.real * rightQuaternion.real + leftQuaternion.imag * rightQuaternion.imag + \
+                   leftQuaternion.imag1 * rightQuaternion.imag1 + leftQuaternion.imag2 * rightQuaternion.imag2
+    imagOfResult = leftQuaternion.real * rightQuaternion.imag - leftQuaternion.imag * rightQuaternion.real - \
+                   leftQuaternion.imag1 * rightQuaternion.imag2 + leftQuaternion.imag2 * rightQuaternion.imag1
+    imag1OfResult = leftQuaternion.real * rightQuaternion.imag1 + leftQuaternion.imag * rightQuaternion.imag2 - \
+                    leftQuaternion.imag1 * rightQuaternion.real - leftQuaternion.imag2 * rightQuaternion.imag
+    imag2OfResult = leftQuaternion.real * rightQuaternion.imag2 - leftQuaternion.imag * rightQuaternion.imag1 + \
+                    leftQuaternion.imag1 * rightQuaternion.imag - leftQuaternion.imag2 * rightQuaternion.real
+
+    return Quaternion(realOfResult / absoluteValueOfLeft,
+                      imagOfResult / absoluteValueOfLeft,
+                      imag1OfResult / absoluteValueOfLeft,
+                      imag2OfResult / absoluteValueOfLeft)
+
+
+def negation(operand):
+    typeOfOperand = type(operand)
+
+    if typeOfOperand is Vector:
+        return negateVector(operand)
+    elif typeOfOperand is Quaternion:
+        return negateQuaternion(operand)
+
+    raise TypeError(str(typeOfOperand) + "s do not have a negation")
+
+
+def negateVector(vector):
+    return vectorTimesReal(vector, -1)
+
+
+def negateQuaternion(quaternion):
+    return quaternionTimesReal(quaternion, -1)
--- cal/Vector.py
+++ cal/Vector.py
@@ -1,3 +1,42 @@
+from math import sqrt, acos
+
+
 class Vector:
     def __init__(self, point):
-        self.point = point
+        self.point = tuple(point)
+
+    def __len__(self):
+        return len(self.point)
+
+    def equalDimensions(self, vector):
+        return len(self) == len(vector)
+
+    def __getitem__(self, index):
+        return self.point[index]
+
+    def dot(self, vector):
+        if not self.equalDimensions(vector):
+            raise ArithmeticError("Two Vectors must be of equal dimensions to have a dot product")
+
+        dotProduct = 0.0
+
+        for value1, value2 in zip(self, vector):
+            dotProduct += value1 * value2
+
+        return dotProduct
+
+    def magnitude(self):
+        mag = 0.0
+
+        for value in self:
+            mag += value ** 2
+
+        mag = sqrt(mag)
+
+        return mag
+
+    def angleBetween(self, vector):
+        return acos(self.dot(vector) / (self.magnitude() * vector.magnitude()))
+
+    def __iter__(self):
+        return self.point.__iter__()
