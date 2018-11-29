0a602070524be1e67ca189de2b4978d7011be470
AHorejsi <alex.horejsi59@gmail.com>
2018-11-29 14:47:38 -0800

    Removed old package

--- cal/__init__.py
+++
@@ -1,5 +0,0 @@
-from cal.BaseConversion import intToBinary, intToHexadecimal, intToOctal, binaryToInt, hexadecimalToInt, octalToInt
-from cal.Matrix import Matrix
-from cal.Complex import Complex
-from cal.Quaternion import Quaternion
-from cal.Vector import Vector
---
+++ calc/_ComplexMediator.py
@@ -0,0 +1,153 @@
+from calc.Complex import Complex
+from calc.Quaternion import Quaternion
+from calc.Matrix import Matrix
+
+
+def _addition(leftComplex, rightOperand):
+    typeOfOperand = type(rightOperand)
+
+    if (typeOfOperand is int) or (typeOfOperand is float):
+        return _complexPlusReal(leftComplex, rightOperand)
+    elif typeOfOperand is Complex:
+        return _complexPlusComplex(leftComplex, rightOperand)
+    elif typeOfOperand is Quaternion:
+        return _complexPlusQuaternion(leftComplex, rightOperand)
+
+
+def _complexPlusReal(leftComplex, rightReal):
+    return Complex(leftComplex.real + rightReal, leftComplex.imag0)
+
+
+def _complexPlusComplex(leftComplex, rightComplex):
+    return Complex(leftComplex.real + rightComplex.real, leftComplex.imag0 + rightComplex.imag0)
+
+
+def _complexPlusQuaternion(leftComplex, rightQuaternion):
+    return Quaternion(leftComplex.real + rightQuaternion.real,
+                      leftComplex.imag0 + rightQuaternion.imag0,
+                      rightQuaternion.imag1,
+                      rightQuaternion.imag2)
+
+
+def _subtraction(leftComplex, rightOperand):
+    typeOfOperand = type(rightOperand)
+
+    if (typeOfOperand is int) or (typeOfOperand is float):
+        return _complexMinusReal(leftComplex, rightOperand)
+    elif typeOfOperand is Complex:
+        return _complexMinusComplex(leftComplex, rightOperand)
+    elif typeOfOperand is Quaternion:
+        return _complexMinusQuaternion(leftComplex, rightOperand)
+
+
+def _complexMinusReal(leftComplex, rightReal):
+    return Complex(leftComplex.real - rightReal, leftComplex.imag0)
+
+
+def _complexMinusComplex(leftComplex, rightComplex):
+    return Complex(leftComplex.real - rightComplex.real, leftComplex.imag0 - rightComplex.imag0)
+
+
+def _complexMinusQuaternion(leftComplex, rightQuaternion):
+    return Quaternion(leftComplex.real - rightQuaternion.real,
+                      leftComplex.imag0 - rightQuaternion.imag0,
+                      -rightQuaternion.imag1,
+                      -rightQuaternion.imag2)
+
+
+def _multiplication(leftComplex, rightOperand):
+    typeOfOperand = type(rightOperand)
+
+    if (typeOfOperand is int) or (typeOfOperand is float):
+        return _complexTimesReal(leftComplex, rightOperand)
+    elif typeOfOperand is Complex:
+        return _complexTimesComplex(leftComplex, rightOperand)
+    elif typeOfOperand is Quaternion:
+        return _complexTimesQuaternion(leftComplex, rightOperand)
+    elif typeOfOperand is Matrix:
+        return _complexTimesMatrix(leftComplex, rightOperand)
+
+
+def _complexTimesReal(leftComplex, rightReal):
+    return Complex(leftComplex.real * rightReal, leftComplex.imag0 * rightReal)
+
+
+def _complexTimesComplex(leftComplex, rightComplex):
+    return Complex(leftComplex.real * rightComplex.real - leftComplex.imag * rightComplex.imag,
+                   leftComplex.real * rightComplex.imag + leftComplex.imag * rightComplex.real)
+
+
+def _complexTimesQuaternion(leftComplex, rightQuaternion):
+    return Quaternion(leftComplex.real * rightQuaternion.real - leftComplex.imag * rightQuaternion.imag,
+                      leftComplex.real * rightQuaternion.imag + rightQuaternion.real * leftComplex.imag,
+                      leftComplex.real * rightQuaternion.imag1 - leftComplex.imag * rightQuaternion.imag2,
+                      leftComplex.real * rightQuaternion.imag2 + leftComplex.imag * rightQuaternion.imag1)
+
+
+def _complexTimesMatrix(leftComplex, rightMatrix):
+    from calc._MatrixMediator import _matrixTimesScalar
+
+    return _matrixTimesScalar(rightMatrix, leftComplex)
+
+
+def _division(leftComplex, rightOperand):
+    typeOfOperand = type(rightOperand)
+
+    if (typeOfOperand is int) or (typeOfOperand is float):
+        return _complexDividedByReal(leftComplex, rightOperand)
+    elif typeOfOperand is Complex:
+        return _complexDividedByComplex(leftComplex, rightOperand)
+    elif typeOfOperand is Quaternion:
+        return _complexDividedByQuaternion(leftComplex, rightOperand)
+
+
+def _complexDividedByReal(leftComplex, rightReal):
+    return _complexTimesReal(leftComplex, 1 / rightReal)
+
+
+def _complexDividedByComplex(leftComplex, rightComplex):
+    conj = rightComplex.conjugate()
+    numerator = _complexTimesComplex(leftComplex, conj)
+    denominator = (rightComplex * conj).real
+
+    return Complex(numerator.real / denominator, numerator.imag0 / denominator)
+
+
+def _complexDividedByQuaternion(leftComplex, rightQuaternion):
+    absoluteValueOfRight = abs(rightQuaternion)
+
+    realOfResult = leftComplex.real * rightQuaternion.real + leftComplex.imag * rightQuaternion.imag
+    imagOfResult = leftComplex.imag * rightQuaternion.real - leftComplex.real * rightQuaternion.imag
+    imag1OfResult = -leftComplex.real * rightQuaternion.imag1 - leftComplex.imag * rightQuaternion.imag2
+    imag2OfResult = leftComplex.imag * rightQuaternion.imag1 - leftComplex.real * rightQuaternion.imag2
+
+    return Quaternion(realOfResult / absoluteValueOfRight,
+                      imagOfResult / absoluteValueOfRight,
+                      imag1OfResult / absoluteValueOfRight,
+                      imag2OfResult / absoluteValueOfRight)
+
+
+def _equality(leftComplex, rightOperand):
+    typeOfOperand = type(rightOperand)
+
+    if (typeOfOperand is int) or (typeOfOperand is float):
+        return _complexEqualsReal(leftComplex, rightOperand)
+    elif typeOfOperand is Complex:
+        return _complexEqualsComplex(leftComplex, rightOperand)
+    elif typeOfOperand is Quaternion:
+        return _complexEqualsQuaternion(leftComplex, rightOperand)
+
+
+def _complexEqualsReal(leftComplex, rightReal):
+    return leftComplex.real == rightReal and leftComplex.imag0 == 0
+
+
+def _complexEqualsComplex(leftComplex, rightComplex):
+    return leftComplex.real == rightComplex.real and leftComplex.imag0 == rightComplex.imag0
+
+
+def _complexEqualsQuaternion(leftComplex, rightQuaternion):
+    return leftComplex.real == rightQuaternion.real and \
+           leftComplex.imag0 == rightQuaternion.imag0 and \
+           rightQuaternion.imag1 == 0 and \
+           rightQuaternion.imag2 == 0
