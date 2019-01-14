56a07154f4a9aa7ff0279cdfc23b20030af322d3
AHorejsi <alex.horejsi59@gmail.com>
2019-01-14 06:10:08 -0800

    Documented the Exponentable class

--- calc/Exponentable.py
+++ calc/Exponentable.py
@@ -1,9 +1,59 @@
 class Exponentable:
-    def __pow__(self, power, modulo=None):
+    """
+    Instances of this class are instances of MathEntity
+    which are capable of exponent operations
+    """
+
+    def __pow__(self, mathEntity, modulo=None):
+        """
+        Takes this mathematical entity to the power of
+        another mathematical entity
+
+        :param mathEntity: The mathematical entity
+            that is acts as the exponent for this
+            operation
+        :param modulo: Another mathematical entity
+            that the result of this mathematical entity
+            taken to the power of another mathematical
+            entity will be modded by
+        :return: The result of taking this mathematical
+            entity to the power of this another mathematical
+            entity
+        """
+
         raise NotImplementedError("This method must be implemented")
 
     def __ipow__(self, mathEntity):
+        """
+        Updates this mathematical entity by taking this
+        mathematical entity to the power of
+        another mathematical entity
+
+        :param mathEntity: The mathematical entity
+            that is acts as the exponent for this
+            operation
+        :return: The result of taking this mathematical
+            entity to the power of this another mathematical
+            entity
+        """
+
         return self ** mathEntity
 
     def __rpow__(self, mathEntity):
+        """
+        Takes another mathematical entity to the power of
+        this mathematical entity
+
+        :param mathEntity: The mathematical entity
+            that is acts as the base for this
+            operation
+        :param modulo: Another mathematical entity
+            that the result of another mathematical entity
+            taken to the power of this mathematical
+            entity will be modded by
+        :return: The result of taking another mathematical
+            entity to the power of this this mathematical
+            entity
+        """
+
         raise NotImplementedError("This method must be implemented")
