a3dac6edd54bdb31152409149e3cbab873dd4a03
AHorejsi <alex.horejsi59@gmail.com>
2019-01-30 12:29:10 -0800

    Remade calc/__init__.py

---
+++ calc/__init__.py
@@ -0,0 +1,13 @@
+from calc.MathEntity import MathEntity
+from calc.Negatable import Negatable
+from calc.Exponentable import Exponentable
+from calc.Complex import Complex
+from calc.Quaternion import Quaternion
+from calc.Vector import Vector
+from calc.Matrix import Matrix
+from calc.ComplexFunction import expComplex, logComplex, log10Complex, sqrtComplex, sinComplex, cosComplex, \
+                                 tanComplex, sinhComplex, coshComplex, tanhComplex, asinComplex, acosComplex, \
+                                 atanComplex, asinhComplex, acoshComplex, atanhComplex, ceilComplex, floorComplex, \
+                                 signumComplex
+from calc.QuaternionFunction import expQuaternion, sqrtQuaternion, logQuaternion, log10Quaternion, ceilQuaternion, \
+                                    floorQuaternion
