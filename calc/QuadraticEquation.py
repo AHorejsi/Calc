1bb4dc06894b1b2f59b79ed601f10673f95c43ab
AHorejsi <ahorejs@calstatela.edu>
2019-04-19 04:27:42 -0700

    Renamed data fields in QuadraticEquation class

--- calc/QuadraticEquation.py
+++ calc/QuadraticEquation.py
@@ -4,62 +4,83 @@ from calc.MathFunction import sqrtMath
 
 
 class QuadraticEquation:
-	def __init__(self, a: Union[int, float], b: Union[int, float], c: Union[int, float]):
-		if a == 0:
+	"""
+	Instances of this class represent quadratic equations.
+	Quadratic equations are of the form (a * x ** 2) + (b * x) + c = 0, where
+	a, b and c are real numbers
+	"""
+
+	def __init__(self, leadingCoefficient: Union[int, float], secondCoefficient: Union[int, float], constant: Union[int, float]):
+		"""
+		Constructs a quadratic equation with the given coefficients
+		
+		:param leadingCoefficient: The value of the leading coefficent. This value cannot be set 
+			to zero
+		:param secondCoefficient: The value of the second coefficient
+		:param constant: The value of the constant variable
+		"""
+	
+		if leadingCoefficient == 0:
 			raise ArithmeticError("Leading coefficient cannot be zero")
 	
-		self.__a = a
-		self.__b = b
-		self.__c = c
+		self.__leadingCoefficient = leadingCoefficient
+		self.__secondCoefficient = secondCoefficient
+		self.__constant = constant
 		
 	@property
-	def a(self) -> Union[int, float]:
-		return self.__a
+	def leadingCoefficient(self) -> Union[int, float]:
+		"""
+		Returns the value of the leading coefficient
+		"""
+	
+		return self.__leadingCoefficient
 		
 	@property
-	def b(self) -> Union[int, float]:
-		return self.__b
+	def secondCoefficient(self) -> Union[int, float]:
+		return self.__secondCoefficient
 		
 	@property
-	def c(self) -> Union[int, float]:
-		return self.__c
+	def constant(self) -> Union[int, float]:
+		return self.__constant
 		
 	@property
 	def vertex(self) -> Tuple[Union[int, float]]:
-		xValue = -self.__b / (2 * self.__a)
-		yValue = (self.__a * xValue ** 2) + (self.__b * xValue) + self.__c
+		xValue = -self.__secondCoefficient / (2 * self.__leadingCoefficient)
+		yValue = (self.__leadingCoefficient * xValue ** 2) + (self.__secondCoefficient * xValue) + self.__constant
 		
 		return (xValue, yValue)
 		
 	@property
 	def discriminant(self) -> Union[int, float]:
-		return (self.__b ** 2) - (4 * self.__a * self.__c)
+		return (self.__secondCoefficient ** 2) - (4 * self.__leadingCoefficient * self.__constant)
 		
 	@property
 	def xValue(self) -> Union[Union[int, float], Tuple[Union[int, float, complex]]]:
 		discriminant = self.discriminant
 		
 		if discriminant == 0:
-			return (-self.__b + sqrtMath(discriminant)) / (2 * self.__a)
+			return (-self.__secondCoefficient + sqrtMath(discriminant)) / (2 * self.__leadingCoefficient)
 		else:
-			value1 = (-self.__b + sqrtMath(discriminant)) / (2 * self.__a)
-			value2 = (-self.__b - sqrtMath(discriminant)) / (2 * self.__a)
+			value1 = (-self.__secondCoefficient + sqrtMath(discriminant)) / (2 * self.__leadingCoefficient)
+			value2 = (-self.__secondCoefficient - sqrtMath(discriminant)) / (2 * self.__leadingCoefficient)
 			
 			return (value1, value2)
 			
 	def __iter__(self) -> Iterator[Union[int, float]]:
-		return iter([self.__a, self.__b, self.__c])
+		return iter([self.__leadingCoefficient, self.__secondCoefficient, self.__constant])
 		
 	def __eq__(self, quadEqn: QuadraticEquation) -> bool:
-		return self.__a == quadEqn.__a and self.__b == quadEqn.__b and self.__c == quadEqn.__c
+		return self.__leadingCoefficient == quadEqn.__leadingCoefficient and \
+		self.__secondCoefficient == quadEqn.__secondCoefficient and \
+		self.__constant == quadEqn.__constant
 		
 	def __ne__(self, quadEqn: QuadraticEquation) -> bool:
 		return not (self == quadEqn)
 		
 	def __str__(self) -> str:
-		strRep = QuadraticEquation.__coefficient(self.__a, "x^2") + \
-				 QuadraticEquation.__coefficient(self.__b, "x") + \
-				 QuadraticEquation.__coefficient(self.__c, "")
+		strRep = QuadraticEquation.__coefficient(self.__leadingCoefficient, "x^2") + \
+				 QuadraticEquation.__coefficient(self.__secondCoefficient, "x") + \
+				 QuadraticEquation.__coefficient(self.__constant, "")
 				 
 		return strRep
 	
