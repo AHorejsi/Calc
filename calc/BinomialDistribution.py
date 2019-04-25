57339731f712f7a8674c45ec48131b608de756fc
AHorejsi <ahorejs@calstatela.edu>
2019-04-25 07:09:24 -0700

    Fixed some bugs in recently added files

--- calc/BinomialDistribution.py
+++ calc/BinomialDistribution.py
@@ -3,33 +3,44 @@ from math import sqrt
 
 
 class BinomialDistribution:
-	def __init__(self, numberOfTrials: int, success: float):
-		if numberOfTrials < 1:
-			raise ArithmeticError("The number of trials must be positive")
-		
-		if success < 0 or success > 1:
-			raise ArithmeticError("The probability of success must be between 0 and 1")
-			
-		self.__numberOfTrials = numberOfTrials
-		self.__success = success
-		
-	@property
-	def numberOfTrials(self) -> int:
-		return self.__numberOfTrials
-		
-	@property
-	def success(self) -> float:
-		return self.__success
-		
-	@property
-	def failure(self) -> float:
-		return 1 - self.__success
-		
-	def mean(self) -> float:
-		return self.__numberOfTrials * self.__success
-		
-	def variance(self) -> float:
-		return self.__numberOfTrials * self.__success * self.failure
-		
-	def standardDeviation(self) -> float:
-		return sqrt(self.variance())
+    def __init__(self, numberOfTrials: int, success: float):
+        if numberOfTrials < 1:
+            raise ArithmeticError("The number of trials must be positive")
+
+        if success < 0 or success > 1:
+            raise ArithmeticError("The probability of success must be between 0 and 1")
+
+        self.__numberOfTrials = numberOfTrials
+        self.__success = success
+
+    @property
+    def numberOfTrials(self) -> int:
+        return self.__numberOfTrials
+
+    @property
+    def success(self) -> float:
+        return self.__success
+
+    @property
+    def failure(self) -> float:
+        return 1 - self.__success
+
+    def mean(self) -> float:
+        return self.__numberOfTrials * self.__success
+
+    def variance(self) -> float:
+        return self.__numberOfTrials * self.__success * self.failure
+
+    def standardDeviation(self) -> float:
+        return sqrt(self.variance())
+
+    def __eq__(self, distribution: BinomialDistribution) -> bool:
+        return self.__numberOfTrials == distribution.__numberOfTrials and self.__success == distribution.__success
+
+    def __ne__(self, distribution: BinomialDistribution) -> bool:
+        return not (self == distribution)
+
+    def __str__(self) -> str:
+        return "(# of trials: " + str(self.__numberOfTrials) + \
+               "), (success: " + str(self.__success) + \
+               "), (failure: " + str(self.failure) + ")"
--- calc/ProbabilityDistribution.py
+++ calc/ProbabilityDistribution.py
@@ -1,11 +1,18 @@
 from __future__ import annotations
 from typing import Union, List, Tuple, Iterator
 from calc.MathFunction import sqrtMath
+from calc.Quaternion import Quaternion
 
 
 class ProbabilityDistribution:
 	def __init__(self, data: List[Tuple[Union[int, float, complex, Quaternion], float]]):
 		self.__data = data
+		
+	def __len__(self) -> int:
+		return len(self.__data)
+		
+	def __getitem__(self, index: int) -> Tuple[Union[int, float, complex, Quaternion], float]:
+		return self.__data[index]
 		
 	def mean(self) -> Union[int, float, complex, Quaternion]:
 		meanValue = 0
@@ -28,8 +35,21 @@ class ProbabilityDistribution:
 	def standardDeviation(self) -> Union[int, float, complex, Quaternion]:
 		return sqrtMath(self.variance())
 		
-	def __iter__(self) -> Iterator[Union[int, float]]:
+	def __iter__(self) -> Iterator[Tuple[Union[int, float, complex, Quaternion], float]]:
 		return iter(self.__data)
+		
+	def __eq__(self, distribution: ProbabilityDistribution) -> bool:
+		if len(self) != len(distribution):
+			return False
+			
+		for (leftEntry, rightEntry) in zip(self, distribution):
+			if leftEntry != rightEntry:
+				return False
+				
+		return True
+		
+	def __ne__(self, distribution: ProbabilityDistribution) -> bool:
+		return not (self == distribution)
 		
 	def __str__(self) -> str:
 		return str(self.__data)
