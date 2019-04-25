19d6e32811bf7a2ffaac5ff96300abfe1bdae800
AHorejsi <ahorejs@calstatela.edu>
2019-04-25 06:24:50 -0700

    Added import statements to __init__.py for new classes

--- calc/__init__.py
+++ calc/__init__.py
@@ -3,6 +3,8 @@ from calc.Quaternion import Quaternion
 from calc.Vector import Vector
 from calc.Matrix import Matrix
 from calc.NumberList import NumberList
+from calc.ProbabilityDistribution import ProbabilityDistribution
+from calc.BinomialDistribution import BinomialDistribution
 from calc.QuaternionFunction import sqrtQuaternion, signumQuaternion, logQuaternion, log10Quaternion, \
                                               expQuaternion
 from calc.MatrixFunction import sqrtMatrix, expMatrix, signumMatrix, sinMatrix, cosMatrix, tanMatrix, \
